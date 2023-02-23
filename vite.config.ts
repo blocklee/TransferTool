8b37faa7b7313c36e2027e4df05ac9df4d8e6c7f
blocklee <block.lee121@gmail.com>
2023-02-24 00:37:16 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:'/meerfansTools/',
+    base:'/TransferTool/',
     plugins: [react()],
     resolve: {
         alias: {
